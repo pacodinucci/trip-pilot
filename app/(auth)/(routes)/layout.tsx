bd0a496970e2903c7d204b7974a0966666bf78fd
pacodinucci <franciscoldinucci@gmail.com>
2024-06-05 16:59:21 -0300

    login background

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,11 +1,20 @@
+import { Merriweather_Sans } from "next/font/google";
+
+const merriweather = Merriweather_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full flex flex-col items-center justify-center space-y-12 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-200 to-blue-800">
-      <h1 className="text-4xl md:text-6xl font-semibold text-white drop-shadow-md">
+    <div className="min-h-screen flex flex-col items-center justify-center space-y-12 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-emerald-200 to-emerald-900">
+      <h1
+        className={`${merriweather.className} text-4xl md:text-6xl font-semibold text-white drop-shadow-md logo font_shadow_xl`}
+      >
         TRIP PLANNER
       </h1>
       {children}
